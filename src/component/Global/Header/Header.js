d08ec00ad6c1c6369728338ea27362c5ceedb11b
unknown <khai.nv8x@gmail.com>
2024-11-15 13:57:15 +0700

    Mô tả thay đổi

--- src/component/Global/Header/Header.js
+++ src/component/Global/Header/Header.js
@@ -8,19 +8,52 @@ import "./Header.css";
 import { UseCart } from "../../context/CartContext";
 
 const Header = () => {
-  const navigate =useNavigate();
-  const {cart}=UseCart();
-  const handleSearch=(e)=>{
+  const navigate = useNavigate();
+  const { cart } = UseCart();
+  const handleSearch = (e) => {
     e.preventDefault();
-    navigate(`/search/${e.target.querySelector("input").value}`)
-};
+    navigate(`/search/${e.target.querySelector("input").value}`);
+  };
   return (
     <Navbar expand="lg" className="header bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand style={{ color: "blue" }}>
-          <Link to="/"><img src={`${process.env.PUBLIC_URL}/hitechlink.png`} alt="" /></Link>
+          <Link to="/">
+            <img src={`${process.env.PUBLIC_URL}/hitechlink.png`} alt="" />
+          </Link>
         </Navbar.Brand>
-
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        {/* <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <NavLink to="/aboutus" className="menu">
+              GIỚI THIỆU
+            </NavLink>
+            <NavLink to="/products" className="menu">
+              SẢN PHẨM
+            </NavLink>
+            <NavLink to="/services" className="menu">
+              DỊCH VỤ
+            </NavLink>
+            <NavLink to="/news" className="menu">
+              TIN TỨC
+            </NavLink>
+            <NavLink to="/contactus" className="menu">
+              LIÊN HỆ
+            </NavLink>
+          </Nav>
+          <Form className="d-flex" onSubmit={handleSearch}>
+            <Form.Control
+              type="search"
+              placeholder="tìm kiếm"
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+        </Navbar.Collapse> */}
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
@@ -30,18 +63,12 @@ const Header = () => {
             <NavLink to="/aboutus" className="menu">
               GIỚI THIỆU
             </NavLink>
-            {/* <NavLink to="/whyus" className="menu">
-              LỢI ÍCH
-            </NavLink> */}
             <NavLink to="/products" className="menu">
               SẢN PHẨM
             </NavLink>
             <NavLink to="/services" className="menu">
               DỊCH VỤ
             </NavLink>
-            {/* <NavLink to="/resource" className="menu">
-              TÀI NGUYÊN
-            </NavLink> */}
             <NavLink to="/news" className="menu">
               TIN TỨC
             </NavLink>
